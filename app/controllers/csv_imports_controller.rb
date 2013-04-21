c4957c7325ef9daace2079b3517404c9a17e0492
ekhatko <evgeniy.khatko@gmail.com>
2013-04-21 20:58:13 +0400

    templates import

--- app/controllers/csv_imports_controller.rb
+++ app/controllers/csv_imports_controller.rb
@@ -6,10 +6,34 @@ class CsvImportsController < ApplicationController
 
   def create
     # TODO: see that stuff isn't imported to wrong project
-    import = CsvExchange::Import.new(params[:file], @project.id,
-                                     @current_user.id, params[:simulate])
-    import.process
-    @log = import.log
+    @log = ''
+    if params[:file] != nil
+      import = CsvExchange::Import.new(params[:file], @project.id,
+                                       @current_user.id, params[:simulate])
+      import.process
+      @log += import.log
+    end
+    if params[:tests_template_file] != nil
+      f = create_upload_file('tests.odt')
+      f.puts params[:tests_template_file].read
+      f.close
+      @log += '</br><span style="background-color:#77ff77">Tests template file uploaded'
+    end
+    if params[:results_template_file] != nil
+      f = create_upload_file('results.odt')
+      f.puts params[:results_template_file].read
+      f.close
+      @log += '</br><span style="background-color:#77ff77">Results template file uploaded'
+    end
+
     render :template => '/import/log'
   end
 end
+
+private
+
+  def create_upload_file(name)
+    upload_dir = "#{Rails.public_path}/#{@project.id.to_s}"
+    FileUtils.mkdir_p  upload_dir, :verbose =>true unless File.exists? upload_dir
+    File.new("#{upload_dir}/#{name}", "w+")
+  end
